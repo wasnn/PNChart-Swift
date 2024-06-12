0ac7945d135f6db053951a6038e7f5fcb21eec38
wasnn <wx2012long@163.com>
2024-06-12 15:07:18 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
-
 let package = Package(
-    name: "PNChartSwift",
-    platforms: [
-        .iOS(.v13)
-    ],
+    name: "PNChart",
     products: [
-        .library(name: "PNChartSwift",
-                 targets: ["PNChartSwift"])
+        .library(
+            name: "PNChart",
+            targets: ["PNChart"]),
+    ],
+    dependencies: [
+
     ],
     targets: [
         .target(
-            name: "PNChartSwift",
-            path: "PNChartSwift",
-            resources: [.copy("PrivacyInfo.xcprivacy")]
-        )
+            name: "PNChart",
+            dependencies: []),
+        .testTarget(
+            name: "PNChartTests",
+            dependencies: ["PNChart"]),
     ]
 )
